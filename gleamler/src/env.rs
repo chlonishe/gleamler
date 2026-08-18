104160380a4878bc0f64c0062cc4345638687e80
Chlon <tsaaaaaa111111@gmail.com>
2026-08-18 16:27:31 +1000

    fix: allow env::send from background threads by passing null as caller_env

--- gleamler/src/env.rs
+++ gleamler/src/env.rs
@@ -107,18 +107,19 @@ impl<'a> Env<'a> {
     /// [enif\_send](https://www.erlang.org/doc/man/erl_nif.html#enif_send).
     #[inline]
     pub fn send(self, pid: &LocalPid, message: impl Encoder) -> Result<(), SendError> {
-        if !is_scheduler_thread() {
-            return Err(SendError);
-        }
-
         if self.kind == EnvKind::ProcessIndependent {
             return Err(SendError);
         }
 
         let message = message.encode(self);
         
-        // Send the message.
-        let res = unsafe { enif_send(self.as_c_arg(), pid.as_c_arg(), ptr::null_mut(), message.as_c_arg()) };
+        let caller_env = if is_scheduler_thread() {
+            self.as_c_arg()
+        } else {
+            ptr::null_mut()
+        };
+
+        let res = unsafe { enif_send(caller_env, pid.as_c_arg(), ptr::null_mut(), message.as_c_arg()) };
 
         if res == 1 {
             Ok(())
