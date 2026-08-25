70198d18aae3cf577ba3434832c651439b6fcefe
Chlon <tsaaaaaa111111@gmail.com>
2026-08-26 00:09:04 +1000

    fix(ffi): correct exception pattern matching for nif_panicked in Erlang

--- src/gleamler_stress_ffi.erl
+++ src/gleamler_stress_ffi.erl
@@ -5,7 +5,7 @@ rescue_panic() ->
     try gleamler_nif_ffi:stress_panic(<<"boom">>) of
         V -> {ok, V}
     catch
-        error:nif_panicked ->
+        error:{nif_panicked, _} ->
             {error, <<"nif_panicked">>};
         error:Reason ->
             {error, list_to_binary(io_lib:format("~p", [Reason]))}
