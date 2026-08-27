b9fd78fe09848a366d79e58d34748c4a6f20b958
Chlon <tsaaaaaa111111@gmail.com>
2026-08-27 20:10:56 +1000

    fix(build): remove manual null terminator from generated symbol strings

--- gleamler/build.rs
+++ gleamler/build.rs
@@ -144,7 +144,7 @@ impl ApiBuilder for WriterBuilder<'_> {
     fn func(&mut self, _ret: &str, name: &str, _args: &str) {
         writeln!(
             self.0,
-            "        filler.write(&mut self.{name}, \"{name}\0\");"
+            "        filler.write(&mut self.{name}, \"{name}\");"
         )
         .unwrap();
     }
